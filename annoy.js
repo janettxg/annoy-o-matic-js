508461dcf415588357f426ad616a6df170bb8e42
janett g <janettxgonzalez@gmail.com>
2018-08-16 22:06:50 -0400

    alert javascript

---
+++ annoy.js
@@ -0,0 +1,23 @@
+var answer = prompt("are we there yet?");
+
+while(answer !== "yes" && answer !=="yeah"){
+     var answer = prompt("are we there yet?");
+ }
+
+ alert("YAY, We made it!")
+
+//  if(answer === "yes"){
+//     alert("YAY, We made it!")
+//  } else {
+//      var answer = prompt("are we there yet?");
+//  }
+
+// version 2
+
+// var answer = prompt("are we there yet?");
+
+// while(answer.indexof("yes") === -1){
+//     var answer = prompt("are we there yet?");
+// }
+
+// alert("YAY, We made it!")
